00dabd334850d13f282a53bb3d4eeadb06dd725a
Puis21 <47056535+Puis21@users.noreply.github.com>
2021-08-29 05:51:37 -0700

    Forogt to add the ability widget created

---
+++ Module5Proj/Source/Module5Proj/UI/AbilityWidget.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+#include "Runtime/UMG/Public/UMG.h"
+
+#include "AbilityWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MODULE5PROJ_API UAbilityWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	//UAbilityWidget(const FObjectInitializer& ObjectInitializer);
+
+	//virtual void NativeConstruct() override;
+
+	//void UpdateAbilitySwapBar();
+
+	//void ResetAbilitySwapBar();
+
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	//class UProgressBar* AbilitySwapBar;
+};
